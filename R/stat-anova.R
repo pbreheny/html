6a6057391e6fd691f2b61ca5dc7e96dc7d7a41a3
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-07-20 16:47:05 -0500

    Fixed .html problems in stat-anova and stat-htmlModel

--- R/stat-anova.R
+++ R/stat-anova.R
@@ -1,15 +1,29 @@
 ## One-way ANOVA
 htmlANOVA <- function(x, y, Data, plotname=x, xlab=x, ylab=y, ...) {
-  filename <- paste0(plotname, ".png")
   require(visreg)
-  g <- factor(Data[,x])
-  r <- Data[,y]
-  fit <- lm(r~g)
-  fit0 <- lm(r~1)
+
+  if (!missing(Data)) {
+    xx <- Data[,x]
+    yy <- Data[,y]
+  } else {
+    xx <- get(x,env=.GlobalEnv)
+    yy <- get(y,env=.GlobalEnv)
+  }
+
+  if (is.character(yy)) yy <- factor(yy)
+  if (is.factor(yy)) yy <- as.numeric(yy)-1
+  ind <- is.finite(xx)
+  xxx <- factor(xx[ind])
+  yyy <- yy[ind]
+
+  fit <- lm(yyy~xxx)
+  fit0 <- lm(yyy~1)
   p <- anova(fit0, fit)[2,6]
-  png(paste0("html/compiled/", filename), 5, 5, "in", res=200)
-  visreg(fit, "g", xlab=xlab, ylab=ylab, ...)
+
+  visfile <- paste0(.html$img, plotname,"-vis.png")
+  png(paste0(.html$dir, visfile), 5, 5, "in", res=200)
+  visreg(fit, "xxx", xlab=xlab, ylab=ylab, ...)
   mtext(formatP(p, label=TRUE), line=0.5)
   dev.off()
-  htmlFig(filename)
+  htmlFig(visfile)
 }
--- R/stat-htmlModel.R
+++ R/stat-htmlModel.R
@@ -1,5 +1,7 @@
-htmlModel <- function(fit, plotname="fit", reverse=FALSE, OR=FALSE, exclude=NULL, standardize=FALSE, sort=TRUE, labels=rownames(B)) {
-  plotfile <- paste(plotname,".png",sep="")
+htmlModel <- function(fit, plotname="fit", reverse=FALSE, xlab="Regression coefficient", exclude=NULL,
+                      standardize=FALSE, sort=TRUE, labels=rownames(B)) {
+  imgfile <- paste0(.html$img, plotname, ".png")
+
   if (standardize) {
     tau <- apply(model.matrix(fit), 2, sd)[-1]
     catg <- apply(model.matrix(fit),2,function(x){length(unique(x))})[-1]
@@ -7,21 +9,26 @@ htmlModel <- function(fit, plotname="fit", reverse=FALSE, OR=FALSE, exclude=NULL
     B <- CIplot(fit, exclude=exclude, plot=FALSE, tau=tau)
   } else B <- CIplot(fit, exclude=exclude, plot=FALSE)
   heightRatio <- .1+nrow(B)/10
-  png(paste("html/compiled/",plotfile,sep=""), height=6*heightRatio, width=6, units="in", res=200)
-  xlab <- if (OR) "Odds ratio" else "Regression coefficient"
+
+  png(paste0(.html$dir, imgfile), height=6*heightRatio, width=6, units="in", res=200)
   CIplot(B, xlab=xlab, sort=sort, axis=FALSE, labels=labels)
-  if (OR) {
+  if (grepl("ratio", xlab, ignore.case=TRUE)) {
     logAxis(1, base=exp(1), disp=2, n=4)
   } else {
     axis(1)
   }
   dev.off()
-  if (OR) {
+
+  if (grepl("odds", xlab, ignore.case=TRUE)) {
     colnames(B)[1] <- "OR"
     B[,1:3] <- exp(B[,1:3])
+  } else if (grepl("hazard", xlab, ignore.case=TRUE)) {
+    colnames(B)[1] <- "HR"
+    B[,1:3] <- exp(B[,1:3])
   }
+
   L <- vector("list",2)
-  L[[1]] <- htmlFig(plotfile, height=600*heightRatio, width=600)
+  L[[1]] <- htmlFig(imgfile, height=600*heightRatio, width=600)
   B[,1:3] <- round(B[,1:3],1)
   B[,4] <- formatP(B[,4])
   if (missing(labels)) {
@@ -30,6 +37,6 @@ htmlModel <- function(fit, plotname="fit", reverse=FALSE, OR=FALSE, exclude=NULL
     rownames(B) <- labels
   }
   L[[2]] <- htmlTable(B)
-    
+
   return(htmlList(L))
 }
