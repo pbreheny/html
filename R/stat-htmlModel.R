6cfa53f318eab124e9e742547668419234a42611
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-09-19 16:34:20 -0500

    stat-htmlModel: Bug fixes

--- R/stat-htmlModel.R
+++ R/stat-htmlModel.R
@@ -1,17 +1,23 @@
 htmlModel <- function(fit, plotname="fit", reverse=FALSE, xlab="Regression coefficient", exclude=NULL,
-                      standardize=FALSE, sort=TRUE, labels=rownames(B)) {
+                      standardize=FALSE, sort=TRUE, labels) {
   imgfile <- paste0(.html$img, plotname, ".png")
 
   if (standardize) {
-    tau <- apply(model.matrix(fit), 2, sd)[-1]
-    catg <- apply(model.matrix(fit),2,function(x){length(unique(x))})[-1]
+    X <- model.matrix(fit)
+    ind <- which(colnames(X) != "(Intercept)")
+    tau <- apply(model.matrix(fit), 2, sd)[ind]
+    catg <- apply(model.matrix(fit),2,function(x){length(unique(x))})[ind]
     tau[catg==2] <- 1
     B <- CIplot(fit, exclude=exclude, plot=FALSE, tau=tau)
   } else B <- CIplot(fit, exclude=exclude, plot=FALSE)
   heightRatio <- .1+nrow(B)/10
 
   png(paste0(.html$dir, imgfile), height=6*heightRatio, width=6, units="in", res=200)
-  CIplot(B, xlab=xlab, sort=sort, axis=FALSE, labels=labels)
+  if (missing(labels)) {
+    CIplot(B, xlab=xlab, sort=sort, axis=FALSE)
+  } else {
+    CIplot(B, xlab=xlab, sort=sort, axis=FALSE, labels=labels)
+  }
   if (grepl("ratio", xlab, ignore.case=TRUE)) {
     logAxis(1, base=exp(1), disp=2, n=4)
   } else {
@@ -29,6 +35,7 @@ htmlModel <- function(fit, plotname="fit", reverse=FALSE, xlab="Regression coeff
 
   L <- vector("list",2)
   L[[1]] <- htmlFig(imgfile, height=600*heightRatio, width=600)
+  ind <- if (sort) order(B[,1], decreasing=TRUE) else 1:nrow(B)
   B[,1:3] <- round(B[,1:3],1)
   B[,4] <- formatP(B[,4])
   if (missing(labels)) {
@@ -36,7 +43,7 @@ htmlModel <- function(fit, plotname="fit", reverse=FALSE, xlab="Regression coeff
   } else {
     rownames(B) <- labels
   }
-  L[[2]] <- htmlTable(B)
+  L[[2]] <- htmlTable(B[ind,])
 
   return(htmlList(L))
 }
